ccf8e2fbddc330a7a71700cb13ca1e70a4fee3e9
Alex <atscott@ucsc.edu>
2020-10-16 12:20:28 -0700

    noon timing bugs squashed

--- mainSpotifyEPD.py
+++ mainSpotifyEPD.py
@@ -26,8 +26,8 @@ in the event of a crash. That bash script can be found here:
 https://github.com/alexthescott/4.2in-ePaper-Spotify-Clock/blob/master/launch_epaper.sh
 """
 
+import time, requests
 import spotipy
-from time import time, sleep
 from random import randint
 from datetime import timedelta, datetime as dt
 from requests import get as getRequest
@@ -60,7 +60,7 @@ def mainLoop():
 
             # Firstly, this is for my own edifice to know how long a loop takes for the Pi
             # Secondly, this is used to 'push' our clock forward such that our clock update is exactly on time
-            start = time()
+            start = time.time()
             
             # OPENWEATHER API CALL
             if temp_tuple == None or countTo5 == 4:
@@ -147,24 +147,20 @@ def mainLoop():
                 epd.display(image_buffer)
 
             # Look @ start variable above. find out how long it takes to compute our image
-            stop = time()
+            stop = time.time()
             time_elapsed = stop - start
 
             remaining_time = sec_left - time_elapsed
             if remaining_time < 0: remaining_time = 60
 
-            if (time_str[-2:] == 'am' and (hour >= 6 and hour != 12)) or (time_str[-2:] == 'pm' and (hour < 8 or hour == 12)):
-                # 6:00am - 7:59pm update screen every 3 minutes
+            if (time_str[-2:] == 'am' and (hour >= 6 and hour != 12)) or (time_str[-2:] == 'pm' and hour <= 12):
+                # 6:00am - 11:59pm update screen every 3 minutes
                 print("\t", round(time_elapsed, 2), "\tseconds per loop\t", "sleeping for {} seconds".format(int(remaining_time) + 120))
                 sleep(remaining_time + 120)
-            elif ((time_str[-2:] == 'am' and (hour < 2)) or time_str[-2:] == 'pm' and (hour >= 8 or hour == 12)):
-                # 7pm - 1:59am update screen every 5ish minutes
+            elif (time_str[-2:] == 'am' and (hour < 2 or hour == 12)):
+                # 12:00am - 1:59am update screen every 5ish minutes
                 print("\t", round(time_elapsed, 2), "\tseconds per loop\t", "sleeping for {} seconds".format(int(remaining_time) + 240))
                 sleep(remaining_time + 240)
-            else:
-                # 12:00am - 7:59am, don't update screen, check time every 15 minutes
-                print("\t", round(time_elapsed, 2), "\tseconds per loop\t", "sleeping for {} seconds".format(int(remaining_time) + 840))
-                sleep(remaining_time + 840)
 
             # Increment counter for Weather requests
             countTo5 = 0 if countTo5 == 4 else countTo5 + 1
@@ -178,7 +174,7 @@ def mainLoop():
                 print("new bug caught\n" + str(e))
                 error_file.write(str(e) + "\n")
         print("Retrying mainLoop() in 20 seconds...")
-        sleep(20)
+        time.sleep(20)
         mainLoop()
 
 # Spotify Functions
@@ -260,19 +256,19 @@ def getTimeFromDatetime(time_elapsed, oldMinute):
 
     # Here we make some considerations so the screen isn't updated too frequently
     # We air on the side of caution, and would rather add an additional minute than shrink by a minute
-    if oldMinute != None and ((time_str[-2:] == 'am' and (hour >= 6 and hour != 12)) or (time_str[-2:] == 'pm' and (hour < 8 or hour == 12))):
-        # 6:00am - 7:59pm update screen every 3 mins
+    if oldMinute != None and ((time_str[-2:] == 'am' and (hour >= 6 and hour != 12)) or (time_str[-2:] == 'pm' and hour <= 12)):
+        # 6:00am - 11:59pm update screen every 3 mins
         while int(abs(oldMinute - newMinute)) < 3:
             date = dt.now() + timedelta(seconds = time_elapsed)
             newMinute = int(date.strftime("%M")[-1])
             sleep(2)
-    # 8:00pm - 2:00am update screen every 5 mins at least
-    elif oldMinute != None and ((time_str[-2:] == 'am' and (hour < 2)) or time_str[-2:] == 'pm' and (hour >= 8 or hour == 12)):
+    # 12:00am - 1:59am update screen every 5 mins at least
+    elif oldMinute != None and (time_str[-2:] == 'am' and (hour < 2 or hour == 12)):
         while int(abs(oldMinute - newMinute)) < 5:
             date = dt.now() + timedelta(seconds = time_elapsed)
             newMinute = int(date.strftime("%M")[-1])
             sleep(2)
-    # 2:00am - 5:59am check time every 15ish minutes, but granularity here is not paramount
+    # 2:00am - 5:59am check time every 15ish minutes, granularity here is not paramount
     sec_left = 60 - int(date.strftime("%S")) 
     date_str, am_pm = date.strftime("%a, %b %-d"), date.strftime("%p")
     time_str = date.strftime("%-I:%M") + am_pm.lower()
@@ -312,7 +308,7 @@ def getWeather():
     # get current weather for user on the right
     OW_CURRENT_URL = "http://api.openweathermap.org/data/2.5/weather?"
     OW_CURRENT_COMPLETE = OW_CURRENT_URL + "appid=" + OW_KEY + "&id=" + OW_OTHER_CITYID + "&units=imperial"
-    weather_response = getRequest(OW_CURRENT_COMPLETE)
+    weather_response = requests.get(OW_CURRENT_COMPLETE)
     weather_json = weather_response.json()
     if weather_json["cod"] != "404":
         other_temp = round(weather_json['main']['feels_like'])
