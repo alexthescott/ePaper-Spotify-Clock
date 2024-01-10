ad4fa2befa77c6e13bb2300268933546f1bc8d17
Alex <atscott@ucsc.edu>
2024-01-10 11:17:05 -0800

    rm STDOUT logging

--- lib/clock_logging.py
+++ lib/clock_logging.py
@@ -7,17 +7,17 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Create handlers
-c_handler = logging.StreamHandler()
+# c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler('clock.log')
-c_handler.setLevel(logging.INFO)
+# c_handler.setLevel(logging.INFO)
 f_handler.setLevel(logging.INFO)
 
 # Create formatters and add it to handlers
 c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-c_handler.setFormatter(c_format)
+# c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
 
 # Add handlers to the logger
-logger.addHandler(c_handler)
+# logger.addHandler(c_handler)
 logger.addHandler(f_handler)
