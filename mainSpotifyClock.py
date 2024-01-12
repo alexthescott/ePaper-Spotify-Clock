837870e40ea8ed9745db188bcc5c31731c6ede8a
Alex <atscott@ucsc.edu>
2024-01-11 21:32:17 -0800

    add --clock parameter for local development

--- mainSpotifyClock.py
+++ mainSpotifyClock.py
@@ -1,10 +1,15 @@
 import logging
+import argparse
 from lib.clock import Clock
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--clock', action='store_true', help='Enable clock on unsupported machine ')
+    args = parser.parse_args()
+
     logging.getLogger().setLevel(logging.INFO)
     clock = Clock()
-    if clock.local_run:
+    if clock.local_run and not args.clock:
         clock.build_image()
         clock.save_local_file()
     else:
