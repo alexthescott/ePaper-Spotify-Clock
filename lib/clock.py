0dc2b80f0e000b4689bd5eef01a72600e228769f
Alex <atscott@ucsc.edu>
2024-01-28 12:14:46 -0800

    protect context RW ops

--- lib/clock.py
+++ lib/clock.py
@@ -97,14 +97,19 @@ class Clock:
             start = time() # Used to 'push' our clock timing forward to account for EPD time
 
             # If we have no context read, grab context our cache/context.txt json file
-            if all([self.ctx_type_1, self.ctx_title_1]) or all([self.ctx_type_2, self.ctx_title_2]):
+            ctx_full_1, ctx_full_2 = all([self.ctx_type_1, self.ctx_title_1]), all([self.ctx_type_2, self.ctx_title_2])
+            if ctx_full_1 or ctx_full_2:
                 try:
                     fh = open("cache/context.txt", encoding="utf-8")
-                    self.ctx_type_1, self.ctx_type_1, self.ctx_type_2, self.ctx_title_2 = self.ctx_io.read_json_ctx((self.ctx_type_1, self.ctx_title_1), (self.ctx_type_2, self.ctx_title_2))
+                    self.ctx_type_1, self.ctx_title_1, self.ctx_type_2, self.ctx_title_2 = self.ctx_io.read_json_ctx((self.ctx_type_1, self.ctx_title_1), (self.ctx_type_2, self.ctx_title_2))
                     fh.close()
                 except (FileNotFoundError, json.JSONDecodeError) as e:
-                    logger.error("cache/context.txt -> %s", e)
-                self.ctx_io.write_json_ctx((self.ctx_type_1, self.ctx_title_1), (self.ctx_type_2, self.ctx_title_2))
+                    logger.error("error reading cache/context.txt -> %s", e)
+            if ctx_full_1 and ctx_full_2:
+                try:
+                    self.ctx_io.write_json_ctx((self.ctx_type_1, self.ctx_title_1), (self.ctx_type_2, self.ctx_title_2))
+                except json.JSONDecodeError as e:
+                    logger.error("error writing cache/context.txt -> %s", e)
                 
             self.build_image()
 
