8ac28eebfe9377fa214f6cfb95958920027cad92
Alex <atscott@ucsc.edu>
2024-03-11 11:06:21 -0700

    fix v2 partial call

--- lib/clock.py
+++ lib/clock.py
@@ -204,9 +204,9 @@ class Clock:
                                 y_end = 288
                                 buffer = self.epd.getbuffer(time_image)
                                 if self.version_2:
-                                    self.epd.EPD_4IN2_PartialDisplay(x_start, y_start, x_end, y_end, buffer)
-                                else:
                                     self.epd.display_Partial(buffer)
+                                else:
+                                    self.epd.EPD_4IN2_PartialDisplay(x_start, y_start, x_end, y_end, buffer)
                             else:
                                 self.build_image(time_str)
                                 self.save_local_file()
